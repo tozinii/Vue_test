48f986951255040f52e1a6dd18c28c234d6192a2
Iker Apaolaza Clemente <iapaolaza@indaba.lks.es>
2019-08-14 14:08:23 +0200

    02 add css classess dynamically.

--- VueTutorial/01/js/01.js
+++ VueTutorial/01/js/01.js
@@ -3,8 +3,13 @@ const app = new Vue({
   data: {
     title: 'Testing Vuejs',
     cars: [ {brand: 'ferrari', fuel: false},
-            {brand: 'seat', fuel: true},
-            {brand: 'ford', fuel: true} 
+            {brand: 'seat', fuel: false},
+            {brand: 'ford', fuel: true},
+            {brand: 'audi', fuel: true},
+            {brand: 'skoda', fuel: false},
+            {brand: 'land rover', fuel: true},
+            {brand: 'jeep', fuel: false},
+            {brand: 'lexus', fuel: true},
   ],
     newCarBrand: '',
     newCarFuel: false,
@@ -16,11 +21,14 @@ const app = new Vue({
         brand: this.newCarBrand, fuel: this.newCarFuel
       });
       console.log('añadido ' + this.newCarBrand + ' ' + this.newCarFuel);
+      this.newCarBrand = '';
+      this.newCarFuel = false;
     },
 
     deleteCar(index){
       this.cars.splice(index,1);
       console.log('Borrado ' + index + ' de la lista.');
+      this.delCarIndex = '';
     }
   }
 })
